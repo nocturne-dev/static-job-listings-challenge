160c1f590308e2f6681fab312e045f72719c46a3
Frankii Tang <frankiitang@fb.com>
2023-05-02 14:42:38 -0700

    Added tailwind

---
+++ src/models/Companies.model.ts
@@ -0,0 +1,15 @@
+export type Company = {
+  id: number;
+  company: string;
+  logo: string;
+  isNew: boolean;
+  isFeatured: boolean;
+  position: string;
+  role: string;
+  level: string;
+  postedAt: string;
+  contract: string;
+  location: string;
+  languages: string[];
+  tools: string[];
+};
